a6b4766aaa7042440399ba1a0b1384a9a5db4f63
karthi <karthiraj040@gmail.com>
2022-10-22 08:09:02 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.scss';
 import Home from './components/Layout/Home'
 import Header from './components/Layout/Header'
-import './components/Layout/Home.scss'
+import './components/Layout/Home.css'
+import './components/Layout/Header.scss'
 import 'bootstrap'
 
 function App() {
